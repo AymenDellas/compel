24c40110b95bdeadb1b04141b50f253da6fa2a26
AymenDellas <aymen.dellas@etu.usthb.dz>
2026-09-23 14:58:02 +0100

    Add Microsoft Clarity tracking to site head

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,6 +80,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <Script id="microsoft-clarity" strategy="beforeInteractive" type="text/javascript">
+          {`
+            (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "ymtpyq0lb1");
+          `}
+        </Script>
+      </head>
       <body
         className={`bg-background text-text ${outfit.variable} ${firaCode.variable} ${outfit.className}`}
       >
